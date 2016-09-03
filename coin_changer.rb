ec887579723b8cee94faec27f8ccb1fe89157c38
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-03 15:48:01 -0400

    refactored again: final

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,9 +1,5 @@
 class CoinChanger
   def make_change(coins)
-    if coins >= 1
-      return [coins / 25, (coins % 25) / 10, ((coins % 25) % 10) / 5, ((coins % 25) % 10) % 5]
-    elsif coins == 0
-      return [0, 0, 0, 0]
-    end
+    coins >= 1 ? [coins / 25, (coins % 25) / 10, ((coins % 25) % 10) / 5, ((coins % 25) % 10) % 5] : [0, 0, 0, 0]
   end
 end
