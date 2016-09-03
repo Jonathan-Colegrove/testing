eb62f1f12a838396acc69a6a44beec06ced4256e
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-03 15:43:47 -0400

    works for all coins except zero

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,15 +1,15 @@
 class CoinChanger
   def make_change(coins)
     if coins == 25
-      return [1]
+      return [1, 0, 0, 0]
     elsif coins == 10
-      return [0, 1]
+      return [0, 1, 0, 0]
     elsif coins == 5
-      return [0, 0, 1]
+      return [0, 0, 1, 0]
     elsif coins == 1
       return [0, 0, 0, 1]
     elsif coins >= 25
-      return [coins / 25, 0, 0, 0]
+      return [coins / 25, (coins % 25) / 10, ((coins % 25) % 10) / 5, ((coins % 25) % 10) % 5]
     end
   end
 end
--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -5,15 +5,15 @@ describe "quarter" do
   it "returns coins[0] == 1 when given a quarter" do
     coin_changer = CoinChanger.new
     coins = coin_changer.make_change(25)
-    assert_equal([1], coins)
+    assert_equal([1, 0, 0, 0], coins)
   end
 end
 
 describe "dime" do
-  it "returns coins[0, 1] == 1 when given a dime" do
+  it "returns coins[0, 1, 0, 0] == 1 when given a dime" do
     coin_changer = CoinChanger.new
     coins = coin_changer.make_change(10)
-    assert_equal([0, 1], coins)
+    assert_equal([0, 1, 0, 0], coins)
   end
 end
 
@@ -21,7 +21,7 @@ describe "nickel" do
   it "returns coins[0, 0, 1] == 1 when given a nickel" do
     coin_changer = CoinChanger.new
     coins = coin_changer.make_change(5)
-    assert_equal([0, 0, 1], coins)
+    assert_equal([0, 0, 1, 0], coins)
   end
 end
 
@@ -40,11 +40,19 @@ describe "quarters" do
     assert_equal([3, 0, 0, 0], coins)
   end
 end
-# 
-# describe "26" do
-#   it "returns coins[1, 0, 0, 1] == 1 when given a quarter & a penny" do
-#     coin_changer = CoinChanger.new
-#     coins = coin_changer.make_change(26)
-#     assert_equal([1, 0, 0, 1], coins)
-#   end
-# end
+
+describe "26" do
+  it "returns coins[1, 0, 0, 1] == 1 when given a quarter & a penny" do
+    coin_changer = CoinChanger.new
+    coins = coin_changer.make_change(26)
+    assert_equal([1, 0, 0, 1], coins)
+  end
+end
+
+describe "67" do
+  it "returns coins[2, 1, 1, 2] when given 67 cents" do
+    coin_changer = CoinChanger.new
+    coins = coin_changer.make_change(67)
+    assert_equal([2, 1, 1, 2], coins)
+  end
+end
