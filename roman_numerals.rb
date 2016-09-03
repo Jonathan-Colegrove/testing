cb509da71eeba2b689721c2ef2aafc0bb93683d9
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-03 16:51:18 -0400

    works with 1

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,4 +1,11 @@
 class ArabicToRoman
-  def switch(arabic)
+  def switch(n)
+    conversion = { 0 => "nulla", 1 => "I", 4 => "IV", 5 => "V", 9 => "IX",
+                   10 => "X", 49 => "XLIX", 50 => "L",
+                   99 => "XCIX", 100 => "C", 500 => "D", 1000 => "M" }
+
+    if n == 1
+      return conversion[1]
+    end
   end
 end
