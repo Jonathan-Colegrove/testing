55728005f3253b5aeb7afa956f02c1a0c577ccac
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-06 17:52:31 -0400

    finished Roman

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,9 +1,21 @@
 class ArabicToRoman
   def switch(n)
-    conversion = { 0 => "nulla", 1 => "I", 4 => "IV", 5 => "V", 9 => "IX",
-                   10 => "X", 49 => "XLIX", 50 => "L",
-                   99 => "XCIX", 100 => "C", 500 => "D", 1000 => "M" }
+    return "nulla" if n.zero?
 
-    conversion[n]
+    conversion = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C",
+                   90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX",
+                   5 => "V", 4 => "IV", 1 => "I"}
+
+    return conversion[n] if conversion[n]
+    roman_result = ""
+
+    conversion.each do |arabic, roman|
+      while n >= arabic
+        roman_result += roman
+        n -= arabic
+      end
+    end
+
+    return roman_result
   end
 end
--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -1,37 +1,44 @@
 require "minitest/autorun"
 require_relative "roman_numerals.rb"
 
-describe "0" do
+describe "roman numerals" do
   it "returns nulla when given 0" do
     result = ArabicToRoman.new.switch(0)
     assert_equal("nulla", result)
   end
-end
 
-describe "1" do
   it "returns I when given 1" do
     result = ArabicToRoman.new.switch(1)
     assert_equal("I", result)
   end
-end
 
-describe "4" do
   it "returns IV when given 4" do
     result = ArabicToRoman.new.switch(4)
     assert_equal("IV", result)
   end
-end
 
-describe "5" do
   it "returns V when given 5" do
     result = ArabicToRoman.new.switch(5)
     assert_equal("V", result)
   end
-end
 
-describe "9" do
+  it "returns VI when given 6" do
+    result = ArabicToRoman.new.switch(6)
+    assert_equal("VI", result)
+  end
+
   it "returns VI when given 9" do
     result = ArabicToRoman.new.switch(9)
     assert_equal("IX", result)
+  end
+
+  it "returns CMLXXVII when given 501" do
+    result = ArabicToRoman.new.switch(501)
+    assert_equal("DI", result)
+  end
+
+  it "returns CMLXXVII when given 977" do
+    result = ArabicToRoman.new.switch(977)
+    assert_equal("CMLXXVII", result)
   end
 end
