8479ac7e165d50ca3647a7f16a2e6cec65cbef81
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-13 16:22:37 -0400

    updating anagram

--- anagram.rb
+++ anagram.rb
@@ -1,3 +1,10 @@
-def anagram(word)
-  []
+def anagram(word, array)
+  list = []
+  array.each do |l|
+    letters = l.split("")
+    if word.split("").sort == letters.sort
+      return word.to_a
+    end
+  end
+  list
 end
--- anagram_test.rb
+++ anagram_test.rb
@@ -3,6 +3,10 @@ require_relative "anagram.rb"
 
 describe "tape" do
   it "returns [] if there aren't any anagrams" do
-    assert_equal [], (anagram %w{fan salamander Rex})
+    assert_equal [], anagram("word", %w{apple banana orange})
+  end
+
+  it "returns orange if the options are apple banana orange" do
+    assert_equal "o", (anagram %w{apple banana orange})
   end
 end
