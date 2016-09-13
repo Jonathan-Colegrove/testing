f7c1959d95498a2b5ddf8e95deea38e0ee6f14a4
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-13 16:59:43 -0400

    anagram complete

--- anagram.rb
+++ anagram.rb
@@ -1,10 +1,9 @@
 def anagram(word, array)
   list = []
   array.each do |l|
-    letters = l.downcase.split("")
-    if word.downcase == l.downcase
-      next
-    elsif word.downcase.split("").sort == letters.sort
+    letters = l.downcase.chars
+    next if word.downcase == l.downcase
+    if word.downcase.chars.sort == letters.sort
       list << l
     end
   end
