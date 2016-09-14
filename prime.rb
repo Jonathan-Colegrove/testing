d1f15f7ef1a1c876f33c166d7c51a6e41594686b
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-14 14:27:01 -0400

    finished all tests

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
 require 'prime'
 
-class PrimeNumber
-  def prime_num(n)
-    PRIMES = (1..1_000_000).lazy.select(&:prime?).take(number).to_a.last
-  end
+def prime_num(n)
+  Prime.take(n).last
 end
--- prime_test.rb
+++ prime_test.rb
@@ -3,12 +3,18 @@ require_relative "prime.rb"
 
 describe "prime" do
   it "returns the nth prime number" do
-    prime = PrimeNumber.new.prime_num(1)
-    assert_equal(2, prime)
+    assert_equal 2, prime_num(1)
   end
 
   it "returns the nth prime number" do
-    prime = PrimeNumber.new.prime_num()
-    assert_equal(2, prime)
+    assert_equal 3, prime_num(2)
+  end
+
+  it "returns the nth prime number" do
+    assert_equal 17, prime_num(7)
+  end
+
+  it "returns the nth prime number" do
+    assert_equal 104_743, prime_num(10_001)
   end
 end
